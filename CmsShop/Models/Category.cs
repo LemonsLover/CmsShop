5631a2f54cf41b2fc6b6dd56c17b5601dc58a018
Yehor Ovseukov <ki201_oeyu@student.ztu.edu.ua>
2021-12-27 14:41:37 +0200

    Adjust regex

--- CmsShop/Models/Category.cs
+++ CmsShop/Models/Category.cs
@@ -10,7 +10,6 @@ namespace CmsShop.Models
     {
         public int Id { get; set; }
         [Required, MinLength(2, ErrorMessage = "Minimun leght is 2")]
-        [RegularExpression(@"^[a-zA-z]+$", ErrorMessage = "Only english letters are allowed")]
         public string Name { get; set;}
         public string Slug { get; set; }
         public int Sorting { get; set; }
